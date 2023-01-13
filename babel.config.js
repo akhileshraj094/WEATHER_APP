f03050b5a963e3e402a559514259af5518dd9f4e
yubinkarki <iamyubinkarki@gmail.com>
2023-01-13 20:19:23 +0545

    Setup environment variables

--- babel.config.js
+++ babel.config.js
@@ -9,5 +9,16 @@ module.exports = {
         },
       },
     ],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
   ],
 };
